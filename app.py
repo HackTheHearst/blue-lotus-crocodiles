4759f07d733093a1358789d2a30311c561da98df
Maggie Shine <magksh@gmail.com>
2014-09-20 21:35:08 -0700

    Refactoring

--- app.py
+++ app.py
@@ -1,13 +1,7 @@
 from flask import Flask, render_template, redirect, url_for, flash, request
-from flask.ext import restful
-from flask.ext.pymongo import PyMongo
 from flask import make_response
-from bson.json_util import dumps
 from jinja2 import Template
-from pprint import pprint
 import json
-from flask import jsonify
-# from flask_rest_service import app
 
 app = Flask(__name__)
 
